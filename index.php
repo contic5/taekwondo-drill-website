e3baec18ea370227d2cd233ab3abac8a2ee3650b
CJ Conti <cjc2818cjc@gmail.com>
2025-07-20 20:39:15 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+<link href="mystyle.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<main>
+<?php require("navbar.php") ?>
+<h1>Taekwondo Drill Database</h1>
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis dui nec tellus ultricies dapibus sit amet sed quam. Vestibulum sit amet semper nisl, sed aliquet mauris. Suspendisse interdum tortor vitae mi viverra, vitae tincidunt odio tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra aliquam libero eu faucibus. Vestibulum convallis congue tempor. Sed suscipit pulvinar tortor, in malesuada leo faucibus ac.</p>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
